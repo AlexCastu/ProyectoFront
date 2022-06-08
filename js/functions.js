e8cf2e9d36aab5298f694f58d1c341152d671a65
AlexCastu <alexcastu@gmail.com>
2022-06-08 20:37:38 +0200

    Solucion casi Final

--- js/functions.js
+++ js/functions.js
@@ -1,4 +1,11 @@
 window.addEventListener('load', () => {
+  if (!localStorage.Acertados) {
+    localStorage.Acertados = 0;
+  } else {
+    localStorage.Acertados = 0;
+  }
+  let cat;
+
   //guardamos algunos de los elementos que usaremos de forma repetitiva
   // Seleccionamos los botones y les añadimos en addeventListener
   document.getElementById('botonCategoriaInformatica').addEventListener('click', () => {
@@ -14,7 +21,14 @@ window.addEventListener('load', () => {
     seleccionarCategoria(10);
   });
 
+  document.getElementById('botonVerHistorial').addEventListener('click', () => mostrarResultadosLocalStorage());
+  document.getElementById('botonBorrarHistorial').addEventListener('click', () => {
+    localStorage.removeItem('SesionesAnterioresPreguntas');
+    location.reload();
+  });
+
   const seleccionarCategoria = (categoria) => {
+    cat = categoria;
     let seccionCategorias = document.getElementById('seccionCategorias');
     seccionCategorias.style.display = 'none';
     mostrarIntro(categoria);
@@ -24,7 +38,7 @@ window.addEventListener('load', () => {
     let div = document.createElement('div');
     div.id = 'intermediarioSeleccionCategoria';
     let botonVolver = document.createElement('button');
-    botonVolver.innerHTML = 'Volver';
+    botonVolver.innerHTML = ' Volver';
     botonVolver.id = 'volverHome';
     let botonEmpezar = document.createElement('button');
     botonEmpezar.id = 'botonJugar';
@@ -78,5 +92,182 @@ window.addEventListener('load', () => {
     }
   };
 
-  const mostrarPreguntasYRespuestas = (preguntas) => {};
+  const mostrarPreguntasYRespuestas = (preguntas) => {
+    let pregunta = [];
+    let respuestas = [];
+    let respuestaCorrecta = [];
+    preguntas.forEach((element) => {
+      pregunta.push(element.question);
+      respuestaCorrecta.push(element.correct_answer);
+      respuestas.push(element.incorrect_answers);
+    });
+
+    logicaPreguntas(pregunta, respuestas, respuestaCorrecta);
+  };
+
+  const logicaPreguntas = (preguntas, respuestasI, respuestaC) => {
+    let respuestas = respuestasI.map((element, index) => {
+      element.push(respuestaC[index]);
+      element.sort();
+      return element;
+    });
+    mostrarTodoPorPantalla(preguntas, respuestas, respuestaC);
+  };
+  const mostrarTodoPorPantalla = (pregunta, respuesta, correcta, id = 0) => {
+    if (document.getElementById('seccionPreguntas')) {
+      const seccion = document.getElementById('seccionPreguntas');
+      id++;
+      seccion.remove();
+      mostrarCard(pregunta, respuesta, correcta, id);
+    } else {
+      let contador = document.createElement('div');
+      contador.id = 'contadorPreguntas';
+      for (let i = 1; i <= 10; i++) {
+        let p = document.createElement('p');
+        p.className = 'numeroPreguntas';
+        p.innerHTML = i;
+        contador.appendChild(p);
+      }
+      document.getElementById('contenedorMain').append(contador);
+      mostrarCard(pregunta, respuesta, correcta, id);
+    }
+  };
+
+  const mostrarCard = (pregunta, respuesta, correcta, id) => {
+    let numeroPregunta = document.querySelectorAll('.numeroPreguntas');
+
+    if (id < 10) {
+      numeroPregunta[id].style.boxShadow = '0px 0px 10px 0px black';
+      let seccion = document.createElement('section');
+      seccion.id = 'seccionPreguntas';
+      let resp = document.createElement('p');
+      resp.innerHTML = pregunta[id];
+      let respS = document.createElement('div');
+      respS.id = 'contenedorRespuestas';
+      respS.append(resp);
+      respuesta[id].forEach((element) => {
+        let opcion = document.createElement('button');
+        opcion.innerHTML = element;
+        opcion.className = 'opcionRespuestas';
+        opcion.value = element;
+        respS.appendChild(opcion);
+      });
+      seccion.append(respS);
+      document.getElementById('contenedorMain').append(seccion);
+      activarBoton(pregunta, respuesta, correcta, id);
+    } else {
+      mostrarResultado();
+    }
+  };
+
+  const activarBoton = (pregunta, respuesta, correcta, id) => {
+    document.querySelectorAll('.opcionRespuestas').forEach((element) => {
+      element.addEventListener('click', () => {
+        if (correcta[id] === element.value) {
+          element.style.boxShadow = '0px 0px 10px 0px green';
+          setTimeout(() => {
+            validarRespuesta(correcta[id], element.value, id);
+            mostrarTodoPorPantalla(pregunta, respuesta, correcta, id);
+          }, 500);
+        } else {
+          element.style.boxShadow = '0px 0px 10px 0px red';
+          setTimeout(() => {
+            validarRespuesta(correcta[id], element.value, id);
+            mostrarTodoPorPantalla(pregunta, respuesta, correcta, id);
+          }, 500);
+        }
+      });
+    });
+  };
+
+  const validarRespuesta = (correcta, respuesta, id) => {
+    let numeroPregunta = document.querySelectorAll('.numeroPreguntas');
+    if (correcta === respuesta) {
+      numeroPregunta[id].style.boxShadow = '0px 0px 10px 0px green';
+      localStorage.Acertados++;
+    } else {
+      numeroPregunta[id].style.boxShadow = '0px 0px 10px 0px red';
+    }
+  };
+
+  const mostrarResultado = () => {
+    let div = document.createElement('div');
+    div.id = 'contenedorResultado';
+    let img = document.createElement('img');
+    img.src = './img/home.gif';
+    img.id = 'logoHome';
+    let p = document.createElement('p');
+    p.innerHTML =
+      localStorage.Acertados > 5 ? `Has respondido ${localStorage.Acertados} bien, Felicidades!` : `Has respondido solo ${localStorage.Acertados} preguntas bien , tienes que esforzarte un poco mas!`;
+    div.append(p, img);
+    document.getElementById('contenedorMain').append(div);
+    document.getElementById('logoHome').addEventListener('click', () => location.reload());
+    guardarDatosLocalStorage();
+  };
+
+  const mostrarResultadosLocalStorage = () => {
+    document.getElementById('contenedorSesionesAnteriores') ? document.getElementById('contenedorSesionesAnteriores').remove() : null;
+    let datos = localStorage.getItem('SesionesAnterioresPreguntas');
+    datos = JSON.parse(datos);
+    datos.length > 5 ? (datos = datos.slice(-5)) : null;
+    const div = document.createElement('div');
+    div.id = 'contenedorSesionesAnteriores';
+    const divTitulo = document.createElement('div');
+    const pAciertos = document.createElement('p');
+    pAciertos.innerHTML = 'Aciertos';
+    const pCategoria = document.createElement('p');
+    pCategoria.innerHTML = 'Categoria';
+    const tiempo = document.createElement('p');
+    tiempo.innerHTML = 'Tiempo';
+    divTitulo.append(pAciertos, pCategoria, tiempo);
+    datos.forEach((element) => {
+      let divEach = document.createElement('div');
+      let pA = document.createElement('p');
+      pA.innerHTML = element.aciertos;
+      let pI = document.createElement('img');
+      if (element.cat === 18) {
+        pI.src = './img/info.png';
+      } else if (element.cat === 11) {
+        pI.src = './img/cine.png';
+      } else if (element.cat === 12) {
+        pI.src = './img/musica.png';
+      } else {
+        pI.src = './img/random.png';
+      }
+      let fecha = document.createElement('p');
+
+      fecha.innerHTML = element.fecha;
+      divEach.append(pA, pI, fecha);
+      div.append(divEach);
+    });
+    let h2 = document.createElement('h2');
+    h2.innerHTML = 'Sesiones anteriores';
+    div.append(divTitulo, h2);
+    var padre = document.getElementById('contenedorMain');
+    var referencia = document.getElementById('seccionCategorias');
+
+    padre.insertBefore(div, referencia);
+  };
+  // Guardamos las sesiones en el local storage tras terminar la tanda de preguntas
+  const guardarDatosLocalStorage = () => {
+    if (localStorage.SesionesAnterioresPreguntas) {
+      let local = JSON.parse(localStorage.SesionesAnterioresPreguntas);
+      let aciertos = localStorage.Acertados;
+      let fecha = new Date().getTime();
+      let guardar = {
+        cat,
+        aciertos,
+        fecha,
+      };
+      local.push(guardar);
+      let objAguardar = JSON.stringify(local);
+      localStorage.setItem('SesionesAnterioresPreguntas', objAguardar);
+    } else {
+      let aciertos = localStorage.Acertados;
+      let fecha = new Date().getTime();
+      let guardar = [{ cat, aciertos, fecha }];
+      let objAguardar = [JSON.stringify(guardar)];
+      localStorage.setItem('SesionesAnterioresPreguntas', objAguardar);
+    }
+  };
 });
